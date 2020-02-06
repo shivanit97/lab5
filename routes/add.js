4259e932663b7e12dd420b1936b19138f8cc690b
shivanit97 <54549438+shivanit97@users.noreply.github.com>
2020-02-06 11:50:13 -0800

    Update add.js

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,9 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
+exports.addFriend = function(request, response) {   
+	data.friends.push({
+	    "name": request.query.name, 
+	    "description": request.query.description,
+		"imageURL": "http://lorempixel.com/500/500/people"}) 
+	response.render('index', data);
  }
